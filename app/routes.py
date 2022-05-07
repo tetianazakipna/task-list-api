0fc75f2779d3a8369932a27e3e0b45b4a3b7c897
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-05-06 21:22:15 -0700

    wave 3 completed

--- app/routes.py
+++ app/routes.py
@@ -1,59 +1,33 @@
 from operator import ne
 from flask import Blueprint, jsonify, request, make_response, abort
+from psycopg2 import Date
 
 from app import db
 from app.models.task import Task
+from datetime import datetime
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-
-@tasks_bp.route("", methods=["POST"])
-def create_task():
-    request_body = request.get_json()
-
-    if "title" not in request_body or "description" not in request_body:
-        return jsonify({"details": f"Invalid data"}), 400
-
-    new_task = Task(
-        title=request_body["title"],
-        description=request_body["description"]
-    )
-    
-    db.session.add(new_task)
-    db.session.commit()
-
-    return{
-        "task":{
-            "id": new_task.task_id,
-            "title": new_task.title,
-            "description": new_task.description,
-            "is_complete": False
-        }
-    }, 201
-
-@tasks_bp.route("", methods=["GET"])
-def get_all_tasks():
-    params = request.args
-
-    if "sort" in params:
-        if params["sort"] == "desc":
-            tasks = Task.query.order_by(Task.title.desc()).all()
-        else:
-            tasks = Task.query.order_by(Task.title).all()
-    else:
-        tasks = Task.query.all()
-        
-    response = []
-    for task in tasks:
-        response.append(
-            {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": False  
+# helper functions to organize the code
+def task_dictionary(task):
+    if task.completed_at is not None:
+        return {
+            "task":{
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": True 
             }
-        )
-    return jsonify(response), 200
+        }
+    else:
+        return {
+            "task":{
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": False 
+            }
+        }
 
 def validate_task(task_id):
     try:
@@ -68,17 +42,58 @@ def validate_task(task_id):
     
     return task
 
-@tasks_bp.route("/<task_id>", methods=["GET"])
-def get_one_task(task_id):
-    task = validate_task(task_id)
-    return {
-        "task":{
+# code to execute routes
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    if "title" not in request_body or "description" not in request_body:
+        return jsonify({"details": f"Invalid data"}), 400
+
+    if "completed_at" in request_body:
+        new_task = Task(
+            title=request_body["title"],
+            description=request_body["description"],
+            completed_at = datetime.utcnow()
+        )
+    else:
+        new_task = Task(
+            title=request_body["title"],
+            description=request_body["description"]
+        )
+    db.session.add(new_task)
+    db.session.commit()
+
+    return task_dictionary(new_task), 201
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    params = request.args
+
+    if "sort" in params:
+        if params["sort"] == "desc":
+            tasks = Task.query.order_by(Task.title.desc()).all()
+        else:
+            tasks = Task.query.order_by(Task.title).all()
+    else:
+        tasks = Task.query.all()
+
+    response = []
+    for task in tasks:
+        response.append(
+            {
             "id": task.task_id,
             "title": task.title,
             "description": task.description,
             "is_complete": False 
-        }
-    }, 200
+            }
+        )
+    return jsonify(response), 200
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id):
+    task = validate_task(task_id)
+    return task_dictionary(task), 200
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_one_task(task_id):
@@ -92,14 +107,7 @@ def update_one_task(task_id):
 
     db.session.commit()
 
-    return {
-        "task":{
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": False 
-        }
-    }, 200
+    return task_dictionary(task), 200
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
@@ -110,3 +118,17 @@ def delete_task(task_id):
 
     return make_response(jsonify({'details': f'Task {task_id} "{task.title}" successfully deleted'}), 200)
 
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_complete_task(task_id):
+    task = validate_task(task_id)
+    task.completed_at = datetime.utcnow()
+    db.session.commit()
+
+    return task_dictionary(task), 200
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_incomplete_task(task_id):
+    task = validate_task(task_id)
+    task.completed_at = None
+    db.session.commit()
+    return task_dictionary(task), 200
--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -5,7 +5,7 @@ from app.models.task import Task
 import pytest
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_incomplete_task(client, one_task):
     # Arrange
     """
@@ -39,10 +39,11 @@ def test_mark_complete_on_incomplete_task(client, one_task):
             "is_complete": True
         }
     }
-    assert Task.query.get(1).completed_at
+    getRes = Task.query.get(1)
+    assert getRes.completed_at
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_on_complete_task(client, completed_task):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -62,7 +63,7 @@ def test_mark_incomplete_on_complete_task(client, completed_task):
     assert Task.query.get(1).completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_completed_task(client, completed_task):
     # Arrange
     """
@@ -99,7 +100,7 @@ def test_mark_complete_on_completed_task(client, completed_task):
     assert Task.query.get(1).completed_at
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_on_incomplete_task(client, one_task):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -119,7 +120,7 @@ def test_mark_incomplete_on_incomplete_task(client, one_task):
     assert Task.query.get(1).completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_missing_task(client):
     # Act
     response = client.patch("/tasks/1/mark_complete")
@@ -128,13 +129,16 @@ def test_mark_complete_missing_task(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
+    assert "details" in response_body
+    assert response_body == {
+        "details": f"No task with id '1' found."
+    }
 
-
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_missing_task(client):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -143,15 +147,19 @@ def test_mark_incomplete_missing_task(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert "details" in response_body
+    assert response_body == {
+        "details": f"No task with id '1' found."
+    }
 
 # Let's add this test for creating tasks, now that
 # the completion functionality has been implemented
-@pytest.mark.skip(reason="No way to test this feature yet")
+
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_task_with_valid_completed_at(client):
     # Act
     response = client.post("/tasks", json={
@@ -181,7 +189,7 @@ def test_create_task_with_valid_completed_at(client):
 
 # Let's add this test for updating tasks, now that
 # the completion functionality has been implemented
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task_with_completed_at_date(client, completed_task):
     # Act
     response = client.put("/tasks/1", json={
