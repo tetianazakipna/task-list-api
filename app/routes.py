c20117eab348b2a88c0c3cefa737df9c7e460193
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-05-08 19:17:46 -0700

    Wave 3 completed

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask import Blueprint, jsonify, abort, make_response, request
 from sqlalchemy import false
 from app import db
 from app.models.task import Task
+from datetime import datetime
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -19,14 +20,24 @@ def validate_task(task_id):
     return task 
 
 def task_dictionary(task):
-    return {
-        "task": {
+    if task.completed_at is None:
+        return {
+            "task": {
             "id":task.task_id,
             "title":task.title,
             "description":task.description,
             "is_complete": False
+            }
         }
-    }
+    else:
+        return {
+            "task": {
+            "id":task.task_id,
+            "title":task.title,
+            "description":task.description,
+            "is_complete": True
+            }
+        }
 
 # route functions
 @tasks_bp.route("", methods=["POST"])
@@ -34,9 +45,13 @@ def add_one_task():
     request_body = request.get_json()
     if "title" not in request_body or "description" not in request_body:
         return jsonify({"details": "Invalid data"}), 400
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"])
-
+    if "completed_at" in request_body:
+        new_task = Task(title=request_body["title"],
+                        description=request_body["description"],
+                        completed_at=request_body["completed_at"])
+    else:
+        new_task = Task(title=request_body["title"],
+                        description=request_body["description"])
     db.session.add(new_task)
     db.session.commit()
 
@@ -84,3 +99,15 @@ def delete_one_task(task_id):
     db.session.delete(task)
     db.session.commit()
     return jsonify({'details':f'Task {task_id} "{task.title}" successfully deleted'}), 200
+
+@tasks_bp.route("/<task_id>/<is_complete>", methods=["PATCH"])
+def mark_task_completed(task_id, is_complete):
+    task = validate_task(task_id)
+    if is_complete == "mark_complete":
+        task.completed_at = datetime.utcnow()
+    elif is_complete == "mark_incomplete":
+        task.completed_at = None
+    else:
+        abort(make_response({"details": f"unsupported command '{is_complete}'"}, 404))
+    db.session.commit()
+    return task_dictionary(task), 200
