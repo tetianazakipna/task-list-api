64347ba3f312d32b41952bb4f6fb6434763bd714
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-05-10 14:21:28 -0700

    Models in db changed and upgraded

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,5 +2,6 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", back_populates="goal")
--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,9 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    goal = db.relationship("Goal", back_populates="tasks")
