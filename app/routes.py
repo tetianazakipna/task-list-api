c6559a17019a4bed478c60d59e9c9f1b99b4316c
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-05-09 18:43:26 -0700

    Wave 5 completed

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,7 @@ import os
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
-# helper functions
+# helper functions for tasks
 def validate_task(task_id):
     try:
         task_id = int(task_id)
@@ -28,23 +28,23 @@ def task_dictionary(task):
     if task.completed_at is None:
         return {
             "task": {
-            "id":task.task_id,
-            "title":task.title,
-            "description":task.description,
-            "is_complete": False
+                "id":task.task_id,
+                "title":task.title,
+                "description":task.description,
+                "is_complete": False
             }
         }
     else:
         return {
             "task": {
-            "id":task.task_id,
-            "title":task.title,
-            "description":task.description,
-            "is_complete": True
+                "id":task.task_id,
+                "title":task.title,
+                "description":task.description,
+                "is_complete": True
             }
         }
 
-# route functions
+# route functions for tasks
 @tasks_bp.route("", methods=["POST"])
 def add_one_task():
     request_body = request.get_json()
@@ -124,4 +124,69 @@ def send_message_to_slack(task):
     message = f"Someone just completed the task '{task.title}'."
     slackkeys = {"channel": "task-notifications", "text": message}
     slack_request = requests.get('https://slack.com/api/chat.postMessage', params=slackkeys, headers=headers)
+
+# helper functions for goals
+def goal_dictionary(goal):
+    return{
+        "goal":{
+            "id":goal.goal_id,
+            "title":goal.title
+        }
+    }
+
+def validate_goal(goal_id):
+    try:
+       goal_id = int(goal_id)
+    except: 
+        abort(make_response({"details": f"Goal id '{goal_id}' is invalid. Goal id should be a number."}, 400))
+    goal = Goal.query.get(goal_id)
+    if not goal:
+        abort(make_response({"details": f"There is no existing goal with id {goal_id}."}, 404))
+    return goal
+
+# route functions for goals
+@goals_bp.route("", methods=["POST"])
+def post_one_goal():
+    request_body = request.get_json()
+    if "title" not in request_body:
+        return jsonify({"details": "Invalid data"}), 400
+    new_goal = Goal(title = request_body["title"])
+    db.session.add(new_goal)
+    db.session.commit()
+    return goal_dictionary(new_goal), 201
+
+@goals_bp.route("", methods=["GET"])
+def get_all_goals():
+    goals = Goal.query.all()
+    response = []
+    for goal in goals:
+        response.append(
+            {
+                "id": goal.goal_id,
+                "title": goal.title
+            }
+        )
+    return jsonify(response), 200
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def get_one_goal(goal_id):
+    goal = validate_goal(goal_id)
+    return goal_dictionary(goal), 200
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_one_goal(goal_id):
+    goal = validate_goal(goal_id)
+    request_body = request.get_json()
+    if "title" not in request_body:
+        return jsonify({"details": f"Request must include title."}), 400
+    goal.title = request_body["title"]
+    db.session.commit()
+    return goal_dictionary(goal)
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_one_goal(goal_id):
+    goal = validate_goal(goal_id)
+    db.session.delete(goal)
+    db.session.commit()
+    return jsonify({'details': f'Goal {goal_id} "{goal.title}" successfully deleted'}), 200
 
