03c6316cca40c8648fca7b2bff9c0d56ded59d67
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-05-11 14:46:32 -0700

    All waves added, all tests passed

--- tests/test_wave_06.py
+++ tests/test_wave_06.py
@@ -100,7 +100,7 @@ def test_get_tasks_for_specific_goal(client, one_task_belongs_to_one_goal):
     }
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_get_task_includes_goal_id(client, one_task_belongs_to_one_goal):
     response = client.get("/tasks/1")
     response_body = response.get_json()
